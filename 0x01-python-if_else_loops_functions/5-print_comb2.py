0586278da4efc4c27c1d7bbddc03dce46382ae3e
Catherine Otieno <otienocatherine00@gmail.com>
2022-09-21 00:14:51 +0300

    correction

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-     if number == 99:
+    if number == 99:
         print("{}".format(number))
-     else:
-        print(f'{number:02d}', end=", ")
-        
+    else:
+        print("{:02}".format(number), end=", ")
